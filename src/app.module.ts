df3a31bbae73ff47841e1488d2b25ac1014c32f5
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-06 15:18:06 -0300

    feat: include env module and config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvModule } from './infra/env/env.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './infra/env/env';
 
 @Module({
-  imports: [],
+  imports: [
+    EnvModule,
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
