5f15cbd83537f3d0c85da1b094a4e838d0efbe22
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-07 17:37:19 -0300

    feat: added Crypthograpy and DatabasePrisma module

--- src/domain/aplication/use-cases/user-use-cases.module.ts
+++ src/domain/aplication/use-cases/user-use-cases.module.ts
@@ -3,9 +3,11 @@ import { DeleteUserUseCase } from './delete-user.use-case';
 import { FindUserByIdUseCase } from './find-user-by-id.use-case';
 import { RegisterUserUseCase } from './register-user.use-case';
 import { EditInfoUserUseCase } from './edit-info-user.use-case';
+import { DatabaseModule } from '@root/infra/database/database.module';
+import { CrypthograpyModule } from '@root/infra/crypthograpy/crypthograpy.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, CrypthograpyModule],
   providers: [DeleteUserUseCase, FindUserByIdUseCase, RegisterUserUseCase, EditInfoUserUseCase],
   exports: [DeleteUserUseCase, FindUserByIdUseCase, RegisterUserUseCase, EditInfoUserUseCase],
 })
