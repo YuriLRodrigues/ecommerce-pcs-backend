f5137cd5c2b174204802cb6bbc1e91930bdab4f3
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-07 17:34:52 -0300

    feat: swagger configs

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { EnvService } from './infra/env/env.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder()
+    .setTitle('PCS Docs')
+    .setDescription('The PCS Ecommerce API description')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      description: 'Enter token',
+      in: 'header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger/docs', app, document);
 
   const configService = app.get(EnvService);
   const PORT = configService.get('PORT');
