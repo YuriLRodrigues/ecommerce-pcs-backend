092e4d5de85be5d3479364fa5e062c395bef1861
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-08 19:43:27 -0300

    feat: added Ecrypter Service

--- src/infra/crypthograpy/crypthograpy.module.ts
+++ src/infra/crypthograpy/crypthograpy.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { HashGenerator } from '@root/domain/aplication/cryptography/hash-generator';
 import { Bcrypt } from './bcrypt';
+import { Encrypter } from '@root/domain/aplication/cryptography/encrypter';
+import { JwtEncrypter } from './jwt-encrypter';
 
 @Module({
   providers: [
+    {
+      provide: Encrypter,
+      useClass: JwtEncrypter,
+    },
     {
       provide: HashGenerator,
       useClass: Bcrypt,
     },
   ],
-  exports: [HashGenerator],
+  exports: [HashGenerator, Encrypter],
 })
 export class CrypthograpyModule {}
