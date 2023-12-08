ad5b3da9194f298eff57e19f213078d995a0515d
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-08 19:35:54 -0300

    feat: added authorization use-case

--- src/domain/aplication/use-cases/user-use-cases.module.ts
+++ src/domain/aplication/use-cases/user-use-cases.module.ts
@@ -5,10 +5,23 @@ import { RegisterUserUseCase } from './register-user.use-case';
 import { EditInfoUserUseCase } from './edit-info-user.use-case';
 import { DatabaseModule } from '@root/infra/database/database.module';
 import { CrypthograpyModule } from '@root/infra/crypthograpy/crypthograpy.module';
+import { AuthorizationUserUseCase } from './authorization.use-case';
 
 @Module({
   imports: [DatabaseModule, CrypthograpyModule],
-  providers: [DeleteUserUseCase, FindUserByIdUseCase, RegisterUserUseCase, EditInfoUserUseCase],
-  exports: [DeleteUserUseCase, FindUserByIdUseCase, RegisterUserUseCase, EditInfoUserUseCase],
+  providers: [
+    DeleteUserUseCase,
+    FindUserByIdUseCase,
+    RegisterUserUseCase,
+    EditInfoUserUseCase,
+    AuthorizationUserUseCase,
+  ],
+  exports: [
+    DeleteUserUseCase,
+    FindUserByIdUseCase,
+    RegisterUserUseCase,
+    EditInfoUserUseCase,
+    AuthorizationUserUseCase,
+  ],
 })
 export class UserUseCasesModule {}
