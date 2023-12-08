b9b40cabaf5f782e18e5536bb3127428ac7f45f4
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-08 19:34:16 -0300

    feat: include auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EnvModule } from './infra/env/env.module';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './infra/env/env';
 import { HttpModule } from './infra/http/http.module';
+import { AuthModule } from './infra/auth/auth.module';
 
 @Module({
   imports: [
     HttpModule,
+    AuthModule,
     EnvModule,
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
