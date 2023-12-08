6e2ffd902c15470e0d461b7554c28a6709f0b046
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-08 19:45:20 -0300

    feat: added userController to use routes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserUseCasesModule } from '@root/domain/aplication/use-cases/user-use-cases.module';
+import { UserController } from '../controller/user-controller';
 
 @Module({
   imports: [UserUseCasesModule],
+  controllers: [UserController],
 })
 export class HttpModule {}
