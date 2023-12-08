cb871480791aaedc82c35593dbe6e61dd32dcb02
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-08 19:44:59 -0300

    added database url, jwt opublic and private key

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -4,6 +4,9 @@ export const envSchema = z.object({
   SERVICE: z.string(),
   VERSION: z.string().default('1.0.0'),
   PORT: z.string().default('3333'),
+  DATABASE_URL: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
+  JWT_PRIVATE_KEY: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
