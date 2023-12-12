2b68d30d1fc653d37a37e2ed8d0b9d510b437dc3
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-12 18:07:05 -0300

    feat: all dtos to swagger docs

--- src/infra/controller/dto/authorization/returns-to-swagger/index.ts
+++ src/infra/controller/dto/authorization/returns-to-swagger/index.ts
@@ -1,9 +1,11 @@
-import { AuthorizationUser200 } from './authorization-user-return-200.dto';
-import { AuthorizationUser401Error } from './authorization-user-return-401.dto';
-import { AuthorizationUser404Error } from './authorization-user-return-404.dto';
+import { AuthorizationUser200DTO } from './authorization-user-200.dto';
+import { AuthorizationUser400DTO } from './authorization-user-400.dto';
+import { AuthorizationUser403DTO } from './authorization-user-403.dto';
+import { AuthorizationUser404DTO } from './authorization-user-404.dto';
 
 export const AuthorizationUserRS = {
-  200: AuthorizationUser200,
-  401: AuthorizationUser401Error,
-  404: AuthorizationUser404Error,
+  200: AuthorizationUser200DTO,
+  400: AuthorizationUser400DTO,
+  403: AuthorizationUser403DTO,
+  404: AuthorizationUser404DTO,
 };
