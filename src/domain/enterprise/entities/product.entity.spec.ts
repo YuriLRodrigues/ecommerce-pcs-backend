36c824ac079f9d17b22694320e639a2f79aecea0
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-12 17:13:22 -0300

    feat: validate in stock and stars

--- src/domain/enterprise/entities/product.entity.spec.ts
+++ src/domain/enterprise/entities/product.entity.spec.ts
@@ -5,32 +5,40 @@ describe('Product - Entity', () => {
   it('should be able to create a product without be in promotion', async () => {
     const product = await ProductEntity.create({
       description: 'Product description',
-      images: ['image-1', 'image-2'],
       onSale: false,
       price: 1500,
+      inStock: true,
+      totalInStock: 2,
+      stars: [1, 5, 3],
     });
 
     expect(product.description).toBe('Product description');
-    expect(product.images.length).toEqual(2);
     expect(product.price).toEqual(1500);
     expect(product.salePrice).toEqual(null);
     expect(product.onSale).toBe(false);
+    expect(product.inStock).toBe(true);
+    expect(product.stars).toEqual(expect.arrayContaining([1, 5, 3]));
+    expect(product.totalInStock).toEqual(2);
   });
 
   it('should be able to create a product with promotion', async () => {
     const product = await ProductEntity.create({
       description: 'Product description with promotion',
-      images: ['image-1', 'image-2', 'image-3'],
       onSale: true,
       price: 1800,
       salePrice: 850,
+      inStock: true,
+      stars: [1, 5, 3],
+      totalInStock: 3,
     });
 
     expect(product.description).toBe('Product description with promotion');
-    expect(product.images.length).toEqual(3);
     expect(product.price).toEqual(1800);
     expect(product.salePrice).toEqual(850);
     expect(product.onSale).toBe(true);
+    expect(product.inStock).toBe(true);
+    expect(product.stars).toEqual(expect.arrayContaining([1, 5, 3]));
+    expect(product.totalInStock).toEqual(3);
   });
 
   it('should be able to create a product with id', async () => {
@@ -39,18 +47,22 @@ describe('Product - Entity', () => {
     const product = await ProductEntity.create(
       {
         description: 'Product description with randomId',
-        images: ['image-1', 'image-2', 'image-3'],
         onSale: true,
         price: 1800,
         salePrice: 850,
+        inStock: true,
+        stars: [1, 4, 5],
+        totalInStock: 5,
       },
       randomId,
     );
 
     expect(product.description).toBe('Product description with randomId');
-    expect(product.images.length).toEqual(3);
     expect(product.price).toEqual(1800);
     expect(product.salePrice).toEqual(850);
     expect(product.onSale).toBe(true);
+    expect(product.inStock).toBe(true);
+    expect(product.stars).toEqual(expect.arrayContaining([1, 4, 5]));
+    expect(product.totalInStock).toEqual(5);
   });
 });
