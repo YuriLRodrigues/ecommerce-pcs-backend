29786ed0bcb6dd233b4890ba6c3c47a082b1077d
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-12 17:14:42 -0300

    feat: authorization dto to swagger docs

--- src/infra/controller/dto/authorization/returns-to-swagger/authorization-user-return-200.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class AuthorizationUser200 {
-  @ApiProperty({
-    description: 'Retorna o sub todas as roles/permissões do usuário',
-  })
-  token: string[];
-}
