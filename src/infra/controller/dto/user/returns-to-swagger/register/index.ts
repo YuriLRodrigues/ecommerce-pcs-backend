1e19b5962436da07e014bdaa72ac6287f75f0f8d
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-13 13:53:12 -0300

    feat: find user by id index swagger docs dto

---
+++ src/infra/controller/dto/user/returns-to-swagger/register/index.ts
@@ -0,0 +1,9 @@
+import { RegisterUser201DTO } from './register-user-201.dto';
+import { RegisterUser400DTO } from './register-user-400.dto';
+import { RegisterUser409DTO } from './register-user-409.dto';
+
+export const RegisterUserRS = {
+  201: RegisterUser201DTO,
+  400: RegisterUser400DTO,
+  409: RegisterUser409DTO,
+};
