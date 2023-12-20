56893482fffa23aab6d66d4585f7363af2a199de
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-20 14:38:38 -0300

    deleted

--- src/infra/controller/dto/user/returns-to-swagger/delete/returns-to-swagger/index.ts
+++
@@ -1,7 +0,0 @@
-import { DeleteUser202DTO } from '../delete-user-202.dto';
-import { DeleteUser404DTO } from '../delete-user-404.dto';
-
-export const DeleteUserRS = {
-  202: DeleteUser202DTO,
-  404: DeleteUser404DTO,
-};
