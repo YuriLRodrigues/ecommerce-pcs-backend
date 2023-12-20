798ae01288fac91b4f12aaeb4aa27985dd9da948
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-20 14:39:50 -0300

    feat: index with all dtos swagger

---
+++ src/infra/controller/dto/user/returns-to-swagger/delete/index.ts
@@ -0,0 +1,11 @@
+import { DeleteUser202DTO } from './delete-user-202.dto';
+import { DeleteUser400DTO } from './delete-user-400.dto';
+import { DeleteUser401DTO } from './delete-user-401.dto';
+import { DeleteUser404DTO } from './delete-user-404.dto';
+
+export const DeleteUserRS = {
+  202: DeleteUser202DTO,
+  400: DeleteUser400DTO,
+  401: DeleteUser401DTO,
+  404: DeleteUser404DTO,
+};
