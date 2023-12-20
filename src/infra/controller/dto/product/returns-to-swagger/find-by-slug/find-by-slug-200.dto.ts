63676e38e6f2537a019d4cc7a26243ad3bfcee30
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-20 14:30:20 -0300

    feat: find by slug swagger dtos

---
+++ src/infra/controller/dto/product/returns-to-swagger/find-by-slug/find-by-slug-200.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class FindProductsBySlug200DTO {
+  @ApiProperty({
+    example: `{name: 'RTX 3080';
+      slug: 'rtx-3080';
+      description: 'Placa de vídeo muito forte, roda todos os jogos em 2K';
+      price: 1599.89;
+      salePrice?: null;
+      onSale: false;
+      createdAt: 14/12/2023;
+      updatedAt?: null;
+      inStock: true;
+      totalInStock: 5;}`,
+    description: 'Retorna o produto contendo todas as suas características',
+  })
+  product: {
+    name: string;
+    slug: string;
+    description: string;
+    price: number;
+    salePrice?: number;
+    onSale: boolean;
+    createdAt: Date;
+    updatedAt?: Date;
+    inStock: boolean;
+    totalInStock: number;
+  };
+}
---
+++ src/infra/controller/dto/product/returns-to-swagger/find-by-slug/find-by-slug-401.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class FindProductsBySlug401DTO {
+  @ApiProperty({
+    example: 'Unauthorized',
+    description: 'Retorna a mensagem de qual foi o erro da determinada requisição',
+  })
+  @IsString()
+  message: string;
+
+  @ApiProperty({
+    example: 401,
+    description: 'Retorna o StatusCode/Identificador do erro',
+  })
+  @IsNumber()
+  statusCode: number;
+}
