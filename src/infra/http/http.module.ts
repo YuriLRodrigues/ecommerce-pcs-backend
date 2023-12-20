904826bb55d8f42f6ffe6eeeb2ed9b1ec110232d
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-20 14:39:13 -0300

    feat: include product controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserUseCasesModule } from '@root/domain/aplication/use-cases/user-use-cases.module';
 import { UserController } from '../controller/user-controller';
+import { UserUseCasesModule } from '@root/domain/aplication/use-cases/user/user-use-cases.module';
+import { ProductController } from '../controller/product.controller';
+import { ProductUseCaseModule } from '@root/domain/aplication/use-cases/product/product-use-cases.module';
 
 @Module({
-  imports: [UserUseCasesModule],
-  controllers: [UserController],
+  imports: [UserUseCasesModule, ProductUseCaseModule],
+  controllers: [UserController, ProductController],
 })
 export class HttpModule {}
