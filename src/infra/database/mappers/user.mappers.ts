601b4891319b0b721af6c8eba04493dd24e560be
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-20 14:40:34 -0300

    feat: user mapper to domain and persistence

--- src/infra/database/mappers/user.mappers.ts
+++ src/infra/database/mappers/user.mappers.ts
@@ -1,23 +1,30 @@
-import { Prisma, User } from '@prisma/client';
-import { UserEntity } from '@root/domain/enterprise/entities/user.entity';
+import { Prisma, User, UserRole } from '@prisma/client';
+import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+import { UserEntity, UserRoles } from '@root/domain/enterprise/entities/user.entity';
 
 export class UserMappers {
   static toDomain(user: User): UserEntity {
-    return UserEntity.create({
-      avatar: user.avatar,
-      email: user.email,
-      name: user.name,
-      password: user.password,
-      username: user.username,
-    });
+    return UserEntity.create(
+      {
+        avatar: user.avatar,
+        email: user.email,
+        name: user.name,
+        password: user.password,
+        username: user.username,
+        roles: user.role.map((role) => UserRoles[role]) ?? [UserRoles.user],
+      },
+      new UniqueEntityId(user.id),
+    );
   }
   static toPersistence(user: UserEntity): Prisma.UserCreateInput {
     return {
+      id: user.id.toValue(),
       avatar: user.avatar,
       email: user.email,
       name: user.name,
       password: user.password,
       username: user.username,
+      role: user.roles.map((role) => UserRole[role]) ?? [UserRole.user],
     };
   }
 }
