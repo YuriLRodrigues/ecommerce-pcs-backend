b4ce26fce164fdee687f0e2c4136bebe1dccc0ab
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-20 14:24:51 -0300

    feat: delete user use-case and test

---
+++ src/domain/aplication/use-cases/user/delete-user.use-case.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-user.repository';
+import { DeleteUserUseCase } from './delete-user.use-case';
+import { UserEntity } from '@root/domain/enterprise/entities/user.entity';
+import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+
+describe('Delete User - Use Case', () => {
+  let sut: DeleteUserUseCase;
+  let inMemoryUsersRepository: InMemoryUsersRepository;
+  let user: UserEntity;
+
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    sut = new DeleteUserUseCase(inMemoryUsersRepository);
+    user = UserEntity.create({
+      avatar: '/avatar.png',
+      name: 'Joe Doe',
+      username: 'joeDoe',
+      email: 'joedoe@hotmail.com',
+      password: '123456789',
+    });
+    inMemoryUsersRepository.register({ user });
+  });
+
+  it('should delete a user with your correctly id', async () => {
+    const output = await sut.execute({ id: user.id });
+
+    expect(output.isRight()).toBe(true);
+    expect(inMemoryUsersRepository.users).toHaveLength(0);
+  });
+
+  it('not should be delete a user with invalid id', async () => {
+    const randomId = new UniqueEntityId();
+
+    const output = await sut.execute({ id: randomId });
+
+    expect(output.isLeft()).toBe(true);
+    expect(inMemoryUsersRepository.users).toHaveLength(1);
+    expect(output.value).toEqual(new Error(`User with id ${randomId} not found`));
+  });
+});
---
+++ src/domain/aplication/use-cases/user/delete-user.use-case.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../../repositories/user.repository';
+import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+import { Either, left, right } from '@root/core/logic/Either';
+
+type Output = Either<Error, null>;
+
+type Input = {
+  id: UniqueEntityId;
+};
+
+@Injectable()
+export class DeleteUserUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute({ id }: Input): Promise<Output> {
+    const user = await this.userRepository.findById({
+      id: id.toValue(),
+    });
+
+    if (!user) {
+      return left(new Error(`User with id ${id} not found`));
+    }
+
+    await this.userRepository.delete({
+      id: id.toValue(),
+    });
+
+    return right(null);
+  }
+}
