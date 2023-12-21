1a8e73bfd97bbd558075fc91e1a047dfa60c4ced
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-21 18:30:47 -0300

    feat: in memory product repository

--- test/repositories/in-memory-product.repository.ts
+++ test/repositories/in-memory-product.repository.ts
@@ -1,15 +1,15 @@
 import {
   DeleteProductProps,
   FindAllProductsProps,
-  FindProductBySlugProps,
   FindProductsByCategoryProps,
+  FindProductBySlugProps,
   ProductRepository,
   RegisterProductProps,
+  SaveProductProps,
 } from '@root/domain/aplication/repositories/product.repository';
 import { ProductEntity } from '@root/domain/enterprise/entities/product.entity';
 
 export class InMemoryProductRepository extends ProductRepository {
-  // constructor(private readonly categoryRepository: CategoryRepository) {}
   public products: ProductEntity[] = [];
 
   async register({ product }: RegisterProductProps): Promise<ProductEntity> {
@@ -17,8 +17,8 @@ export class InMemoryProductRepository extends ProductRepository {
     return product;
   }
 
-  async delete({ id }: DeleteProductProps): Promise<void> {
-    this.products = this.products.filter((prod) => prod.id.toValue() !== id);
+  async delete({ productSlug }: DeleteProductProps): Promise<void> {
+    this.products = this.products.filter((prod) => prod.slug !== productSlug);
     return;
   }
 
@@ -27,12 +27,19 @@ export class InMemoryProductRepository extends ProductRepository {
     return productBySlug ?? null;
   }
 
-  async findProductByCategory({ slug }: FindProductsByCategoryProps): Promise<ProductEntity[]> {
-    const productByCategory = this.products.find((prod) => prod.productCategoryId === slug);
-    return productByCategory ?? null;
-  }
-
   async findAllProducts({ limit, page }: FindAllProductsProps): Promise<ProductEntity[]> {
     return this.products.slice((page - 1) * limit, limit * page);
+  }
+
+  async save({ product }: SaveProductProps): Promise<void> {
+    const productIndex = await this.products.findIndex((prod) => prod.id === product.id);
+
+    product[productIndex] = product;
+    return;
+  }
+
+  async findProductsByCategory({ categorySlug }: FindProductsByCategoryProps): Promise<ProductEntity[]> {
+    const products = await this.products.filter((p) => p.categoryId === categorySlug);
+    return products ?? null;
   }
 }
