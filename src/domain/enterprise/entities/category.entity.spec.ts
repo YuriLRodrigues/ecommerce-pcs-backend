36bd22a3886177540433d85d55a0492b2ba12e8c
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-21 18:27:49 -0300

    feat: category entity and tests

--- src/domain/enterprise/entities/category.entity.spec.ts
+++ src/domain/enterprise/entities/category.entity.spec.ts
@@ -1,6 +1,5 @@
 import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
 import { CategoryEntity } from './category.entity';
-import { ProductEntity } from './product.entity';
 
 describe('Category - Entity', () => {
   it('should be able to create a category with only your name and de rest automatic', () => {
@@ -24,38 +23,6 @@ describe('Category - Entity', () => {
     expect(category.id).toBeInstanceOf(UniqueEntityId);
     expect(category.name).toBe('PCS-2');
     expect(category.slug).toBe('pcss2');
-    expect(category.createdAt).toBeInstanceOf(Date);
-  });
-
-  it('should be able to create a category with products', () => {
-    const product = ProductEntity.create({
-      name: 'Product',
-      slug: 'product',
-      description: 'Product description',
-      price: 1500,
-    });
-
-    const category = CategoryEntity.create({
-      name: 'PCS-2',
-      slug: 'pcss2',
-      createdAt: new Date(),
-      products: [product],
-    });
-
-    expect(category.id).toBeInstanceOf(UniqueEntityId);
-    expect(category.name).toBe('PCS-2');
-    expect(category.slug).toBe('pcss2');
-    expect(category.products).toHaveLength(1);
-    expect(category.products).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          name: 'Product',
-          slug: 'product',
-          description: 'Product description',
-          price: 1500,
-        }),
-      ]),
-    );
     expect(category.createdAt).toBeInstanceOf(Date);
   });
 });
--- src/domain/enterprise/entities/category.entity.ts
+++ src/domain/enterprise/entities/category.entity.ts
@@ -1,5 +1,4 @@
 import { Entity } from '@root/core/domain/entity/entity';
-import { ProductEntity } from './product.entity';
 import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
 import { Optional } from '@root/core/logic/Optional';
 import { createSlug } from '@root/utils/create-slug';
@@ -7,7 +6,6 @@ import { createSlug } from '@root/utils/create-slug';
 export type CategoryEntityProps = {
   name: string;
   slug: string;
-  products: Array<ProductEntity>;
   createdAt: Date;
 };
 
@@ -18,22 +16,16 @@ export class CategoryEntity extends Entity<CategoryEntityProps> {
   get slug() {
     return this.props.slug;
   }
-  get products() {
-    return this.props.products;
-  }
+
   get createdAt() {
     return this.props.createdAt;
   }
 
-  public static create(
-    props: Optional<CategoryEntityProps, 'createdAt' | 'products' | 'slug'>,
-    id?: UniqueEntityId,
-  ) {
+  public static create(props: Optional<CategoryEntityProps, 'createdAt' | 'slug'>, id?: UniqueEntityId) {
     const category = new CategoryEntity(
       {
         createdAt: props.createdAt ?? new Date(),
         name: props.name,
-        products: props.products ?? [],
         slug: props.slug ?? createSlug(props.name),
       },
       id,
