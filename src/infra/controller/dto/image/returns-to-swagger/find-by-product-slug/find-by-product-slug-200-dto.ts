9e65bbb0666f81134d2cd342d9146ee78cda0fa2
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-11 18:42:44 -0300

    feat: find by product slug returns swagger

---
+++ src/infra/controller/dto/image/returns-to-swagger/find-by-product-slug/find-by-product-slug-200-dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class FindByProductSlug200DTO {
+  @ApiProperty({
+    example: 'bucket-url-image-name',
+    description: 'Retorna a url da imagem retornada do bucket',
+  })
+  @IsString()
+  url: string;
+
+  @ApiProperty({
+    example: '122dsu-2323aie',
+    description: 'Id do produto caso aquela imagem esteja ligado a um produto',
+  })
+  @IsString()
+  productId?: string;
+
+  @ApiProperty({
+    example: '122dsu-2323aie',
+    description: 'Id da categoria caso aquela imagem esteja ligada a uma categoria',
+  })
+  @IsString()
+  categoryId?: string;
+}
---
+++ src/infra/controller/dto/image/returns-to-swagger/find-by-product-slug/index.ts
@@ -0,0 +1,11 @@
+import { FindByProductSlug200DTO } from './find-by-product-slug-200-dto';
+import { FindByProductSlug400DTO } from './find-by-product-slug-400.dto';
+import { FindByProductSlug401DTO } from './find-by-product-slug-401.dto';
+import { FindByProductSlug404DTO } from './find-by-product-slug-404.dto';
+
+export const FindByProductSlugRS = {
+  200: FindByProductSlug200DTO,
+  400: FindByProductSlug400DTO,
+  401: FindByProductSlug401DTO,
+  404: FindByProductSlug404DTO,
+};
