a6897fd53883efbd18026f4a702507c1bf6b88a9
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-11 18:21:59 -0300

    feat: product details entity and test

---
+++ src/domain/enterprise/entities/product-details.spec.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+
+import { ImageEntity } from './image.entity';
+import { ProductDetailsEntity } from './product-details';
+
+describe('Category Details - Entity', () => {
+  it('should be able to create a product details with image', () => {
+    const image = ImageEntity.create({
+      url: 'test',
+    });
+
+    const categoryDetails = ProductDetailsEntity.create({
+      name: 'Product Test',
+      description: 'test description',
+      images: [image],
+      price: 100,
+    });
+
+    expect(categoryDetails.id).toBeInstanceOf(UniqueEntityId);
+    expect(categoryDetails.images).toHaveLength(1);
+    expect(categoryDetails.name).toBe('Product Test');
+    expect(categoryDetails.slug).toBe('product-test');
+    expect(categoryDetails.price).toBe(100);
+    expect(categoryDetails.inStock).toBe(false);
+    expect(categoryDetails.onSale).toBe(false);
+    expect(categoryDetails.salePrice).toBe(undefined);
+    expect(categoryDetails.totalInStock).toEqual(undefined);
+  });
+});
