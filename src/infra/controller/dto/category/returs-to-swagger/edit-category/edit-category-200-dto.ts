e15c1c51f254f5cf3cf33f2c1d6c65170d9273e4
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-11 18:37:23 -0300

    feat: edit category returns swagger

---
+++ src/infra/controller/dto/category/returs-to-swagger/edit-category/edit-category-200-dto.ts
@@ -0,0 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsObject, IsString } from 'class-validator';
+
+export class EditCategory200DTO {
+  @ApiProperty({
+    example: 'u12da3-23q',
+    description: 'Id da categoria, sendo ele um UUID',
+  })
+  @IsString()
+  id: string;
+
+  @ApiProperty({
+    example: 'PCS',
+    description: 'Retora o nome da categoria',
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: 'pcs',
+    description: 'Retorna o slug do produto, que é o nome do mesmo, sem caracteres especiais e espaços',
+  })
+  @IsString()
+  slug: string;
+
+  @ApiProperty({
+    example: {
+      url: 'image-name',
+      id: 'u12vdu-25b',
+    },
+    description: 'Nome da imagem e o seu id',
+  })
+  @IsObject()
+  image: {
+    url: string;
+    id: string;
+  };
+}
---
+++ src/infra/controller/dto/category/returs-to-swagger/edit-category/index.ts
@@ -0,0 +1,11 @@
+import { EditCategory200DTO } from './edit-category-200-dto';
+import { EditCategory400DTO } from './edit-category-400-dto';
+import { EditCategory401DTO } from './edit-category-401.dto';
+import { EditCategory404DTO } from './edit-category-404-dto';
+
+export const EditCategoryRS = {
+  200: EditCategory200DTO,
+  400: EditCategory400DTO,
+  401: EditCategory401DTO,
+  404: EditCategory404DTO,
+};
