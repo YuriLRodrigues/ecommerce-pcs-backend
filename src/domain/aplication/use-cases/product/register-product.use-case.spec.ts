86b14a799939f37f0f35308f0581e6751b6af798
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-15 15:07:35 -0300

    feat: register product use case and test

--- src/domain/aplication/use-cases/product/register-product.use-case.spec.ts
+++ src/domain/aplication/use-cases/product/register-product.use-case.spec.ts
@@ -1,10 +1,16 @@
-import { InMemoryProductRepository } from 'test/repositories/in-memory-product.repository';
-import { RegisterProductUseCase } from './register-product.use-case';
+import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+import { ImageEntity } from '@root/domain/enterprise/entities/image.entity';
 import { ProductEntity } from '@root/domain/enterprise/entities/product.entity';
+import { InMemoryImagesRepository } from 'test/repositories/in-memory-images-repository';
+import { InMemoryProductRepository } from 'test/repositories/in-memory-product.repository';
+
+import { RegisterProductUseCase } from './register-product.use-case';
 
 describe('Add Product - Use Case', () => {
   let sut: RegisterProductUseCase;
   let inMemoryProductRepository: InMemoryProductRepository;
+  let inMemoryImagesRepository: InMemoryImagesRepository;
+
   const product: ProductEntity = ProductEntity.create({
     name: 'Product 1',
     description: 'product-1',
@@ -13,10 +19,20 @@ describe('Add Product - Use Case', () => {
 
   beforeEach(() => {
     inMemoryProductRepository = new InMemoryProductRepository();
-    sut = new RegisterProductUseCase(inMemoryProductRepository);
+    inMemoryImagesRepository = new InMemoryImagesRepository();
+    sut = new RegisterProductUseCase(inMemoryProductRepository, inMemoryImagesRepository);
     inMemoryProductRepository.register({
       product,
     });
+    for (let i = 0; i < 3; i++) {
+      const image = ImageEntity.create({
+        url: `image-id-${i}`,
+        productId: new UniqueEntityId(`product-2`),
+      });
+      inMemoryImagesRepository.create({
+        image,
+      });
+    }
   });
 
   it('should be able to register a new product', async () => {
@@ -28,20 +44,12 @@ describe('Add Product - Use Case', () => {
       inStock: true,
       salePrice: 80,
       totalInStock: 10,
+      imagesIds: [],
     });
 
     expect(output.isRight()).toBe(true);
     expect(inMemoryProductRepository.products).toHaveLength(2);
-    expect(output.value).toEqual(
-      expect.objectContaining({
-        description: 'product-2',
-        onSale: true,
-        price: 100,
-        inStock: true,
-        salePrice: 80,
-        totalInStock: 10,
-      }),
-    );
+    expect(output.value).toEqual(inMemoryProductRepository.products[1]);
     expect(inMemoryProductRepository.products[1]).toEqual(
       expect.objectContaining({
         description: 'product-2',
@@ -54,7 +62,32 @@ describe('Add Product - Use Case', () => {
     );
   });
 
-  it('not should be able to create a product with exists slug', async () => {
+  it('should be able to register a new product with images ids', async () => {
+    const output = await sut.execute({
+      id: new UniqueEntityId('product-2'),
+      name: 'Product 2',
+      description: 'product-2',
+      price: 100,
+      imagesIds: [new UniqueEntityId('image-id-1')],
+    });
+
+    expect(output.isRight()).toBe(true);
+    expect(inMemoryProductRepository.products).toHaveLength(2);
+    expect(output.value).toEqual(inMemoryProductRepository.products[1]);
+    expect(inMemoryProductRepository.products[1]).toEqual(
+      expect.objectContaining({
+        slug: 'product-2',
+        description: 'product-2',
+        price: 100,
+        inStock: false,
+        onSale: false,
+        salePrice: undefined,
+        totalInStock: undefined,
+      }),
+    );
+  });
+
+  it('should not be able to create a product with exists slug', async () => {
     const output = await sut.execute({
       name: 'Product 1',
       slug: 'product-1',
--- src/domain/aplication/use-cases/product/register-product.use-case.ts
+++ src/domain/aplication/use-cases/product/register-product.use-case.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { ProductRepository } from '../../repositories/product.repository';
-import { ProductEntity } from '@root/domain/enterprise/entities/product.entity';
+import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
 import { Either, left, right } from '@root/core/logic/Either';
+import { ProductEntity } from '@root/domain/enterprise/entities/product.entity';
+import { createSlug } from '@root/utils/create-slug';
+
+import { ImagesRepository } from '../../repositories/images.repository';
+import { ProductRepository } from '../../repositories/product.repository';
 
 type Input = {
+  id?: UniqueEntityId;
   name: string;
   slug?: string;
   description: string;
@@ -12,15 +17,20 @@ type Input = {
   onSale?: boolean;
   inStock?: boolean;
   totalInStock?: number;
+  imagesIds?: Array<UniqueEntityId>;
 };
 
 type Output = Either<Error, ProductEntity>;
 
 @Injectable()
 export class RegisterProductUseCase {
-  constructor(private readonly productRepository: ProductRepository) {}
+  constructor(
+    private readonly productRepository: ProductRepository,
+    private readonly imagesRepository: ImagesRepository,
+  ) {}
 
   async execute({
+    id,
     description,
     onSale,
     price,
@@ -29,24 +39,42 @@ export class RegisterProductUseCase {
     totalInStock,
     name,
     slug,
+    imagesIds,
   }: Input): Promise<Output> {
     const productExists = await this.productRepository.findProductBySlug({
-      slug,
+      slug: slug ?? createSlug(name),
     });
 
     if (productExists) {
       return left(new Error('Product with this name or slug already exists'));
     }
 
-    const product = ProductEntity.create({
-      name,
-      slug,
-      description,
-      onSale,
-      price,
-      salePrice,
-      inStock,
-      totalInStock,
+    const product = ProductEntity.create(
+      {
+        name,
+        slug,
+        description,
+        onSale,
+        price,
+        salePrice,
+        inStock,
+        totalInStock,
+      },
+      id,
+    );
+
+    const allImagesWithCorrectlyId = await this.imagesRepository.findManyByIds({
+      ids: imagesIds,
+    });
+
+    const imagesWithProductId = allImagesWithCorrectlyId.filter((img) => {
+      img.productId = product.id;
+
+      return img;
+    });
+
+    await this.imagesRepository.saveMany({
+      images: imagesWithProductId,
     });
 
     const newProduct = await this.productRepository.register({
