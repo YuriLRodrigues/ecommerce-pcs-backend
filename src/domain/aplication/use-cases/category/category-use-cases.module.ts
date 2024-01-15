d39d9a745a3533db65b4cbee1e6a7475703db1f7
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-15 15:06:28 -0300

    feat; category use cases module

--- src/domain/aplication/use-cases/category/category-use-cases.module.ts
+++ src/domain/aplication/use-cases/category/category-use-cases.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CreateCategoryUseCase } from './create-category.use-case.spec';
+import { DatabaseModule } from '@root/infra/database/database.module';
+
+import { CreateCategoryUseCase } from './create-category.use-case';
 import { DeleteCategoryUseCase } from './delete-category.use-case';
 import { EditCategoryUseCase } from './edit-category.use-case';
 import { FindAllCategoriesUseCase } from './find-all-categories.use-case';
 import { FindCategoryByIdUseCase } from './find-category-by-id.use-case';
 import { FindCategoryBySlugUseCase } from './find-category-by-slug.use-case';
-import { DatabaseModule } from '@root/infra/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
