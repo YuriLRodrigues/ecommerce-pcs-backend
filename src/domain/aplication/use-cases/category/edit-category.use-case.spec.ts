c811c47e14e34884176d976fe9a189a2e27545ac
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-15 15:06:12 -0300

    feat: edit category use case and test

--- src/domain/aplication/use-cases/category/edit-category.use-case.spec.ts
+++ src/domain/aplication/use-cases/category/edit-category.use-case.spec.ts
@@ -1,18 +1,23 @@
-import { InMemoryCategoryRepository } from 'test/repositories/in-memory-category.repository';
-import { CategoryEntity } from '@root/domain/enterprise/entities/category.entity';
 import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+import { CategoryEntity } from '@root/domain/enterprise/entities/category.entity';
+import { InMemoryCategoryRepository } from 'test/repositories/in-memory-category.repository';
+import { InMemoryImagesRepository } from 'test/repositories/in-memory-images-repository';
+
 import { EditCategoryUseCase } from './edit-category.use-case';
 
 describe('Edit Category Info - Use Case', () => {
   let inMemoryCategoryRepository: InMemoryCategoryRepository;
+  let inMemoryImagesRepository: InMemoryImagesRepository;
   let sut: EditCategoryUseCase;
+
   const category = CategoryEntity.create({
     name: 'PCS',
   });
 
   beforeEach(() => {
-    inMemoryCategoryRepository = new InMemoryCategoryRepository();
-    sut = new EditCategoryUseCase(inMemoryCategoryRepository);
+    inMemoryImagesRepository = new InMemoryImagesRepository();
+    inMemoryCategoryRepository = new InMemoryCategoryRepository(inMemoryImagesRepository);
+    sut = new EditCategoryUseCase(inMemoryCategoryRepository, inMemoryImagesRepository);
     inMemoryCategoryRepository.create({
       category,
     });
@@ -21,11 +26,12 @@ describe('Edit Category Info - Use Case', () => {
   it('should be able to edit an existing category by your id', async () => {
     const output = await sut.execute({
       categoryId: category.id,
+      imagesIds: [],
       name: 'Consoles',
     });
 
     expect(output.isRight()).toBe(true);
-    expect(inMemoryCategoryRepository.category).toHaveLength(1);
+    expect(inMemoryCategoryRepository.categories).toHaveLength(1);
     expect(output.value).toEqual(
       expect.objectContaining({
         name: 'Consoles',
@@ -34,14 +40,15 @@ describe('Edit Category Info - Use Case', () => {
     );
   });
 
-  it('should be able to edit an existing category with invalid id', async () => {
+  it('should not be able to edit an existing category with invalid id', async () => {
     const randomId = new UniqueEntityId();
+
     const output = await sut.execute({
       categoryId: randomId,
     });
 
     expect(output.isLeft()).toBe(true);
-    expect(output.value).toEqual(new Error(`This category does not exist`));
-    expect(inMemoryCategoryRepository.category).toHaveLength(1);
+    expect(output.value).toEqual(new Error(`This category doesn't exist`));
+    expect(inMemoryCategoryRepository.categories).toHaveLength(1);
   });
 });
--- src/domain/aplication/use-cases/category/edit-category.use-case.ts
+++ src/domain/aplication/use-cases/category/edit-category.use-case.ts
@@ -1,29 +1,59 @@
-import { Either, left, right } from '@root/core/logic/Either';
-import { CategoryRepository } from '../../repositories/category.repository';
 import { UniqueEntityId } from '@root/core/domain/entity/unique-id.entity';
+import { Either, left, right } from '@root/core/logic/Either';
 import { CategoryEntity } from '@root/domain/enterprise/entities/category.entity';
 import { createSlug } from '@root/utils/create-slug';
+
+import { CategoryRepository } from '../../repositories/category.repository';
+import { ImagesRepository } from '../../repositories/images.repository';
 
 type Input = {
   categoryId: UniqueEntityId;
   name?: string;
   slug?: string;
   updatedAt?: Date;
+  imagesIds?: Array<UniqueEntityId>;
 };
 
 type Output = Either<Error, CategoryEntity>;
 
 export class EditCategoryUseCase {
-  constructor(private readonly categoryRepository: CategoryRepository) {}
+  constructor(
+    private readonly categoryRepository: CategoryRepository,
+    private readonly imagesRepository: ImagesRepository,
+  ) {}
 
-  async execute({ categoryId, name, slug, updatedAt }: Input): Promise<Output> {
+  async execute({ categoryId, name, slug, updatedAt, imagesIds }: Input): Promise<Output> {
     const categoryExists = await this.categoryRepository.findById({
       categoryId: categoryId.toValue(),
     });
 
     if (!categoryExists) {
-      return left(new Error(`This category does not exist`));
+      return left(new Error(`This category doesn't exist`));
     }
+
+    const imagesByCategoryId = await this.imagesRepository.findByCategoryId({
+      categoryId,
+    });
+
+    const imagesIdsByCategory = imagesByCategoryId.map((img) => img.id);
+
+    const imagesOnDeleteId = imagesIdsByCategory.filter(
+      (originalId) => !imagesIds.find((id) => originalId.toValue() === id.toValue()),
+    );
+
+    const imagesOnSaveId = imagesIds.filter(
+      (originalId) => !imagesIdsByCategory.find((id) => originalId.toValue() === id.toValue()),
+    );
+
+    const findImagesOnSave = await this.imagesRepository.findManyByIds({
+      ids: imagesOnSaveId,
+    });
+
+    const imagesOnSave = findImagesOnSave.map((img) => {
+      img.categoryId = categoryId;
+
+      return img;
+    });
 
     categoryExists.editInfo({
       name,
@@ -31,9 +61,19 @@ export class EditCategoryUseCase {
       updatedAt,
     });
 
-    await this.categoryRepository.save({
+    const callDelete = this.imagesRepository.deleteMany({
+      imagesIds: imagesOnDeleteId,
+    });
+
+    const callSaveImg = this.imagesRepository.saveMany({
+      images: imagesOnSave,
+    });
+
+    const callSaveCat = this.categoryRepository.save({
       category: categoryExists,
     });
+
+    await Promise.all([callDelete, callSaveImg, callSaveCat]);
 
     return right(categoryExists);
   }
