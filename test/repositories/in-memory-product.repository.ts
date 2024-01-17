75e5fe83291acea483965e79c0fd2f1909e5e53e
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-17 17:45:42 -0300

    feat: in memory product repository

--- test/repositories/in-memory-product.repository.ts
+++ test/repositories/in-memory-product.repository.ts
@@ -6,36 +6,36 @@ import {
   ProductRepository,
   RegisterProductProps,
   SaveProductProps,
+  FindProductByIdProps,
 } from '@root/domain/aplication/repositories/product.repository';
 import { ProductEntity } from '@root/domain/enterprise/entities/product.entity';
+import { createSlug } from '@root/utils/create-slug';
 
-export class InMemoryProductRepository extends ProductRepository {
+export class InMemoryProductRepository implements ProductRepository {
   public products: ProductEntity[] = [];
 
   async register({ product }: RegisterProductProps): Promise<ProductEntity> {
     await this.products.push(product);
+
     return product;
   }
 
   async delete({ productSlug }: DeleteProductProps): Promise<void> {
     this.products = this.products.filter((prod) => prod.slug !== productSlug);
+
     return;
+  }
+
+  async findProductById({ id }: FindProductByIdProps): Promise<ProductEntity> {
+    const productById = this.products.find((prod) => prod.id === id);
+
+    return productById ?? null;
   }
 
   async findProductBySlug({ slug }: FindProductBySlugProps): Promise<ProductEntity> {
     const productBySlug = this.products.find((prod) => prod.slug === slug);
+
     return productBySlug ?? null;
-  }
-
-  async findAllProducts({ limit, page }: FindAllProductsProps): Promise<ProductEntity[]> {
-    return this.products.slice((page - 1) * limit, limit * page);
-  }
-
-  async save({ product }: SaveProductProps): Promise<void> {
-    const productIndex = await this.products.findIndex((prod) => prod.id === product.id);
-
-    product[productIndex] = product;
-    return;
   }
 
   async findProductsByCategory({
@@ -47,6 +47,37 @@ export class InMemoryProductRepository extends ProductRepository {
     const products = await this.products.filter((p) => p.categoryId === categoryId);
 
     const productsPaginated = products.slice((page - 1) * limit, limit * page);
+
     return productsPaginated.filter((prod) => prod.inStock === inStock);
+  }
+
+  async findAllProducts({ limit, page, inStock, search }: FindAllProductsProps): Promise<ProductEntity[]> {
+    if (search) {
+      const productsSearched = this.products.filter((prod) => prod.slug === createSlug(search));
+
+      if (inStock) {
+        const productsFiltered = productsSearched.filter((prod) => prod.inStock === inStock);
+
+        return productsFiltered.slice((page - 1) * limit, limit * page);
+      }
+
+      return productsSearched.slice((page - 1) * limit, limit * page);
+    }
+
+    if (inStock) {
+      const productsFiltered = this.products.filter((prod) => prod.inStock === inStock);
+
+      return productsFiltered.slice((page - 1) * limit, limit * page);
+    }
+
+    return this.products.slice((page - 1) * limit, limit * page);
+  }
+
+  async save({ product }: SaveProductProps): Promise<void> {
+    const productIndex = await this.products.findIndex((prod) => prod.id === product.id);
+
+    this.products[productIndex] = product;
+
+    return;
   }
 }
