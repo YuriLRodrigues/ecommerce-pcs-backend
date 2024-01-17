fb2e8a7330ef326786847d2faa3337b18b517fb5
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-17 17:45:10 -0300

    feat: in memory category repository

--- test/repositories/in-memory-category.repository.ts
+++ test/repositories/in-memory-category.repository.ts
@@ -1,41 +1,69 @@
 import {
   CategoryRepository,
-  createProductInCategoryProps,
-  deleteProductInCategoryProps,
-  findCategoryByIdProps,
-  findCategoryBySlugProps,
-  saveProductInCategoryProps,
+  CreateProps,
+  DeleteProps,
+  FindByIdProps,
+  FindBySlugProps,
+  SaveProps,
 } from '@root/domain/aplication/repositories/category.repository';
+import { CategoryDetailsEntity } from '@root/domain/enterprise/entities/category-details.entity';
 import { CategoryEntity } from '@root/domain/enterprise/entities/category.entity';
 
-export class InMemoryCategoryRepository extends CategoryRepository {
-  public category: CategoryEntity[] = [];
+import { InMemoryImagesRepository } from './in-memory-images-repository';
 
-  async createCategory({ category }: createProductInCategoryProps): Promise<CategoryEntity> {
-    this.category.push(category);
+export class InMemoryCategoryRepository implements CategoryRepository {
+  constructor(private readonly inMemoryImagesRepository: InMemoryImagesRepository) {}
+
+  public categories: CategoryEntity[] = [];
+
+  async create({ category }: CreateProps): Promise<CategoryEntity> {
+    this.categories.push(category);
+
     return category;
   }
 
-  async findCategoryBySlug({ categorySlug }: findCategoryBySlugProps): Promise<CategoryEntity> {
-    const category = await this.category.find((cat) => cat.slug === categorySlug);
+  async findBySlug({ categorySlug }: FindBySlugProps): Promise<CategoryEntity> {
+    const category = await this.categories.find((cat) => cat.slug === categorySlug);
+
     return category ?? null;
   }
 
-  async saveCategory({ category }: saveProductInCategoryProps): Promise<void> {
-    const categoryIndex = await this.category.findIndex((cat) => cat.id === category.id);
+  async save({ category }: SaveProps): Promise<void> {
+    const categoryIndex = await this.categories.findIndex((cat) => cat.id === category.id);
 
-    category[categoryIndex] = category;
+    this.categories[categoryIndex] = category;
+
     return;
   }
 
-  async deleteCategory({ categorySlug }: deleteProductInCategoryProps): Promise<void> {
-    await this.category.filter((cat) => cat.slug === categorySlug);
+  async delete({ categoryId }: DeleteProps): Promise<void> {
+    this.categories = this.categories.filter((cat) => cat.id.toValue() !== categoryId);
+
     return;
   }
 
-  async findCategoryById({ categoryId }: findCategoryByIdProps): Promise<CategoryEntity> {
-    const category = await this.category.find((cat) => cat.id.toValue() === categoryId);
+  async findById({ categoryId }: FindByIdProps): Promise<CategoryEntity> {
+    const category = await this.categories.find((cat) => cat.id.toValue() === categoryId);
 
     return category ?? null;
+  }
+
+  async findAll(): Promise<CategoryDetailsEntity[]> {
+    const images = this.inMemoryImagesRepository.images;
+
+    const categoryDetails = this.categories.map((cat) => {
+      const image = images.find((img) => img.categoryId.toValue() === cat.id.toValue());
+
+      return CategoryDetailsEntity.create({
+        image: image && {
+          url: image.url,
+          id: image.id,
+        },
+        name: cat.name,
+        slug: cat.slug,
+      });
+    });
+
+    return categoryDetails;
   }
 }
